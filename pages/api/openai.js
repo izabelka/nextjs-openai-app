b1c544150c20009c69bd0fbb22842a78a205fd63
izabelka <i.konstanciak@gmail.com>
2023-10-06 13:54:29 -0700

    fix file structure and api endpoint

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -1,14 +1,12 @@
-import { OpenAIApi, Configuration } from "openai";
+import OpenAI from 'openai';
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
-
-const openai = new OpenAIApi(configuration);
 
 export default async (req, res) => {
   if (req.body.prompt !== undefined) {
-    const completion = await openai.createCompletion({
+    const completion = await openai.completions.create({
       model: "text-davinci-003",
       prompt: `${req.body.prompt}`,
     });
